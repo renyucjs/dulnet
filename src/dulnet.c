d967bb96069c32a02e32611b320f812d3d82c129
kk <kimson@hp.desktop>
2021-09-27 21:06:44 +0800

    增加epoll实现，方便调试使用

--- src/dulnet.c
+++ src/dulnet.c
@@ -17,8 +17,10 @@
 #include <unistd.h>
 #include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <net/if.h>
@@ -83,6 +85,19 @@ static int tcp_listen(char *ip, int port, char *dev_name)
     return skt;
 }
 
+static int echo(int fd)
+{
+    char buff[1024];
+    int len = recv(fd, buff, sizeof(buff)-1, 0);
+    if(len)
+    {
+        send(fd, buff, len, 0);
+    }
+    buff[len] = 0;
+    printf("rec: %s\r\n", buff);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     char *localip = NULL;
@@ -117,13 +132,63 @@ int main(int argc, char *argv[])
     }
     
     int sfd = tcp_listen(localip, port, netdev);
+    int epfd = epoll_create (100);
+    struct epoll_event wait_evts[32];
+    struct epoll_event epevt;
+
+    epevt.events = EPOLLIN;
+    epevt.data.fd = sfd;
+    if(epoll_ctl(epfd, EPOLL_CTL_ADD , sfd, &epevt) != 0)
+    {
+        printf ("epoll_ctl fail (%s)\n", strerror(errno));
+        exit (EXIT_FAILURE);
+    }
+
     while (1)
     {
-        int fd = accept(sfd, NULL, NULL);
-        close(fd);
-        printf("accept[%s]: %d\r\n", netdev, fd);
+        int nfds = epoll_wait(epfd, wait_evts, sizeof(wait_evts)/sizeof(wait_evts[0]), -1);
+        if(nfds < 0)
+        {
+            printf ("epoll_wait fail (%s)\n", strerror(errno));
+            exit (EXIT_FAILURE);
+        }
+        
+        for(int i = 0; i < nfds; i++)
+        {
+            if((wait_evts[i].events & EPOLLERR) || (wait_evts[i].events & EPOLLHUP) || !(wait_evts[i].events & EPOLLIN))
+            {
+                close(wait_evts[i].data.fd);
+                printf ("epoll_wait fail at fd: %d\n", wait_evts[i].data.fd);
+                exit (EXIT_FAILURE);
+            }
+
+            if(wait_evts[i].data.fd == sfd)//new accept
+            {
+                int fd = accept(sfd, NULL, NULL);
+                if(fd < 0)
+                {
+                    printf ("accept fail (%s)\n", strerror(errno));
+                    exit (EXIT_FAILURE);
+                }
+                //append to epoll
+                epevt.data.fd = fd;
+                epevt.events = EPOLLIN|EPOLLET;
+                if(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &epevt) != 0)
+                {
+                    printf ("epoll_ctl fail (%s)\n", strerror(errno));
+                    exit (EXIT_FAILURE);
+                }
+                printf("accept[%s]: %d\r\n", netdev, fd);
+            }
+            else
+            {
+                echo(wait_evts[i].data.fd);
+            }
+        }
     }
     
+    close(sfd);
+    close(epfd);
 }
 
 /*---------------------------------------------------------------------------*/
